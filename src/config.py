92dd9a13a86a89f069ab59cee114eef727487782
Guillem Hurault <guillem.hurault@hotmail.fr>
2024-08-02 17:48:59 +0100

    Configure palettes and plotting styles

--- src/config.py
+++ src/config.py
@@ -1,6 +1,18 @@
+"""Configurations."""
+
+# %%
+# Imports
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from plotnine import theme_set, theme_bw
+
+# %%
+# Paths
+
 try:
     from pyprojroot import here
-    # Safer to use here() than relying on relative path
+    # Safer to use here() than relying on relative path...
 
     PROJ_ROOT = here()
 except ModuleNotFoundError:
@@ -10,3 +22,45 @@ except ModuleNotFoundError:
 
 DATA_DIR = PROJ_ROOT / "data"
 RES_DIR = PROJ_ROOT / "results"
+
+# %%
+# Palette and plotting styles
+
+CB_PALETTE = (
+    "#999999",
+    "#E69F00",
+    "#56B4E9",
+    "#009E73",
+    "#F0E442",
+    "#0072B2",
+    "#D55E00",
+    "#CC79A7",
+)
+CB_PALETTE = sns.color_palette(CB_PALETTE).as_hex()
+
+CBB_PALETTE = (
+    "#000000",
+    "#E69F00",
+    "#56B4E9",
+    "#009E73",
+    "#F0E442",
+    "#0072B2",
+    "#D55E00",
+    "#CC79A7",
+)
+CBB_PALETTE = sns.color_palette(CBB_PALETTE).as_hex()
+
+theme_set(theme_bw(base_size=15))
+# Update Matplotlib (and seaborn) defaults
+sns.set_palette(CB_PALETTE)
+plt.rcParams.update(
+    {
+        "axes.grid.which": "major",
+        "axes.grid": True,
+        "grid.color": "#EFEFEF",
+        "grid.linewidth": 1,
+        "figure.dpi": 300,
+    }
+)
+
+# %%
