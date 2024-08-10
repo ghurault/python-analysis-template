591a33775e20fad21af0fe0c7f828628542b5e6a
Guillem Hurault <guillem.hurault@hotmail.fr>
2024-08-10 20:45:19 +0100

    Setup logger

--- src/config.py
+++ src/config.py
@@ -6,13 +6,22 @@ Usage:
 """
 
 # %%
-# Import
+# Preamble
+
+import logging
 
 from pyprojroot import here
+
+# logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 # %%
 # Paths
 
 PROJ_ROOT = here()
+
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
 DATA_DIR = PROJ_ROOT / "data"
 RES_DIR = PROJ_ROOT / "results"
