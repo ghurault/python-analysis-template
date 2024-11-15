dff3d2d397c6d680e4c39b82a6cddfab84d7617e
Guillem Hurault <guillem.hurault@hotmail.fr>
2024-11-15 23:16:24 +0000

    Add colour palettes

--- src/config.py
+++ src/config.py
@@ -32,3 +32,21 @@ logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RES_DIR = PROJ_ROOT / "results"
+
+# %%
+# Palettes
+
+CB_PALETTE = [
+    "#999999",
+    "#E69F00",
+    "#56B4E9",
+    "#009E73",
+    "#F0E442",
+    "#0072B2",
+    "#D55E00",
+    "#CC79A7",
+]
+CBB_PALETTE = CB_PALETTE.copy()
+CBB_PALETTE[0] = "#000000"
+
+# %%
