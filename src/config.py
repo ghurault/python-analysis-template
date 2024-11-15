2913c8ef646c9587d39e330111f2a1b1c7345c07
Guillem Hurault <guillem.hurault@hotmail.fr>
2024-11-15 23:01:26 +0000

    Update logging config

--- src/config.py
+++ src/config.py
@@ -12,9 +12,16 @@ import logging
 
 from pyprojroot import here
 
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="{asctime} - {levelname} - {name} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+# The global config should not be redefine
+# The config can still be overriden in custom loggers
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 # %%
 # Paths
